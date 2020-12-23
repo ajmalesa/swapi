1865060c699cbc627b2fe9cb3d327b5de88770e9
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-12-22 17:25:19 -0700

    Fixed formatting issues and catch get requests

--- app/js/script.js
+++ app/js/script.js
@@ -1,35 +1,30 @@
 for (let j = 1; j <= 3; j++) {
     axios.get('https://swapi.dev/api/starships/?page=' + j)
         .then((response) => {
-
             response.data.results.forEach((starship, i) => {
-
-                document.querySelector
-
                 document.querySelector("#list-of-starships").innerHTML += `
                     <li class="starship">
                         ${starship.name}
-
-
                     </li>
                     <ol id="starships-for-${i * j}"></ol>
-                    `;
+                `;
 
                 starship.pilots.forEach((pilot) => {
                     axios.get(pilot).then((response) => {
-                        console.log(response)
-
                         document.querySelector("#starships-for-" + (i * j)).innerHTML += `
-                    <li>
-                        ${response.data.name}
-                    </li>
-
-                
-                `;
+                            <li>
+                                ${response.data.name}
+                            </li>
+                        `;
+                    }).catch((err) => {
+                        document.querySelector("#starships-for-" + (i * j)).innnerHTML = "Could not get pilot. Contact administrator.";
+                        console.log(err);
                     })
                 });
-
             });
 
+        }).catch((err) => {
+            document.querySelector("#list-of-starships").innerHTML = "Could not get starship. Contact administrator.";
+            console.error(err)
         });
 }
