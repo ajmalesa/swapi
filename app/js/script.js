cae4ed7e17455d4ec75010de7a7a4b3f7eb69718
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-12-24 04:24:47 -0700

    Added loading icon and using accordion for list
    - Loading icon appears and fades out as data loads and fades in
    - Using accordion for List as it looks more organized

--- app/js/script.js
+++ app/js/script.js
@@ -10,9 +10,12 @@ function getNextPageOfStarships(url) {
         for (let i = 0; i < response.data.results.length; i++) {
             document.querySelector("#list-of-starships").innerHTML += `
                     <li class="starship">
-                        ${response.data.results[i].name}
+                        <a class="uk-accordion-title">${response.data.results[i].name}</a>
+                        <div class="pilots-for-starships uk-accordion-content" id="pilots-for-${i + url[url.length-1]}"></div>
+                        <hr class="divider">
                     </li>
-                    <ul class="pilots-for-starships" id="pilots-for-${i + url[url.length-1]}"></ul>
+                    
+
                 `;
 
             for (let j = 0; j < response.data.results[i].pilots.length; j++) {
@@ -20,24 +23,40 @@ function getNextPageOfStarships(url) {
                     .then((pilot) => {
 
                         document.querySelector("#pilots-for-" + i + url[url.length - 1]).innerHTML += `
-                                <li>
-                                    ${pilot.data.name}
-                                </li>
+                                <div>
+                                    <p>${pilot.data.name}</p>
+                                </div>
                             `;
                     })
+            }
+
+            if (response.data.results[i].pilots.length === 0) {
+                document.querySelector("#pilots-for-" + i + url[url.length - 1]).innerHTML += `
+                <div>
+                    No pilots found
+                </div>
+            `;
             }
         }
 
         // As long as there is a next page of starships, recursively call function to get next page
         if (response.data.next !== null) {
             getNextPageOfStarships(response.data.next);
+        } else {
+            document.querySelectorAll(".divider")[document.querySelectorAll(".divider").length - 1].remove();
+            document.querySelector(".main-container").classList.remove("uk-hidden");
+            document.querySelector(".main-container").classList.add("fade-in");
+            document.querySelector(".loading-container").classList.add("fade-out");
         }
 
     })
 
     // If we are not able to get a response, let the user know and log the error to make it easy to find in devtools
     .catch((err) => {
-        document.querySelector("#list-of-starships").innerHTML = "Could not get starship. Contact administrator.";
+        document.querySelector(".loading-container").classList.add("fade-out");
+        document.querySelector(".main-container").classList.add("fade-in");
+        document.querySelector(".main-container").classList.remove("uk-hidden");
+        document.querySelector("#list-of-starships").innerHTML = "<span class='uk-text-danger'>Could not get starships. Contact administrator.</span>";
         console.error(err);
     });
 }
