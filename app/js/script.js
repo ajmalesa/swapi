ece6ed577d3f55b2cf71d5523e578620f35b5832
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-12-24 05:26:26 -0700

    Added paginated version

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,14 @@
 // Initial page to look for starships
-let initialPage = "https://swapi.dev/api/starships/?page=1"
+let currentPage = 1;
+let initialPage = "https://swapi.dev/api/starships/?page=" + currentPage;
 
 // Function we will call with url of the next page of starships
 function getNextPageOfStarships(url) {
+
+    // Reset content of current page on new page request
+    document.querySelector("#list-of-starships").innerHTML = "";
+    document.querySelector(".main-container").classList.add("uk-hidden");
+
     axios.get(url)
 
     // If we get a response, append to page
@@ -14,8 +20,6 @@ function getNextPageOfStarships(url) {
                         <div class="pilots-for-starships uk-accordion-content" id="pilots-for-${i + url[url.length-1]}"></div>
                         <hr class="divider">
                     </li>
-                    
-
                 `;
 
             for (let j = 0; j < response.data.results[i].pilots.length; j++) {
@@ -39,16 +43,21 @@ function getNextPageOfStarships(url) {
             }
         }
 
-        // As long as there is a next page of starships, recursively call function to get next page
-        if (response.data.next !== null) {
-            getNextPageOfStarships(response.data.next);
+        if (response.data.previous !== null) {
+            document.querySelector(".previous-button").removeAttribute("disabled");
         } else {
-            document.querySelectorAll(".divider")[document.querySelectorAll(".divider").length - 1].remove();
-            document.querySelector(".main-container").classList.remove("uk-hidden");
-            document.querySelector(".main-container").classList.add("fade-in");
-            document.querySelector(".loading-container").classList.add("fade-out");
+            document.querySelector(".previous-button").setAttribute("disabled", "");
+        }
+        if (response.data.next !== null) {
+            document.querySelector(".next-button").removeAttribute("disabled");
+        } else {
+            document.querySelector(".next-button").setAttribute("disabled", "");
         }
 
+        document.querySelectorAll(".divider")[document.querySelectorAll(".divider").length - 1].remove();
+        document.querySelector(".main-container").classList.remove("uk-hidden");
+        document.querySelector(".main-container").classList.add("fade-in");
+        document.querySelector(".loading-container").classList.add("fade-out");
     })
 
     // If we are not able to get a response, let the user know and log the error to make it easy to find in devtools
@@ -63,3 +72,15 @@ function getNextPageOfStarships(url) {
 
 // Call function defined above, which will keep being called as long as there are more pages of starships
 getNextPageOfStarships(initialPage);
+
+document.querySelector(".next-button").addEventListener("click", () => {
+    currentPage += 1;
+    let nextPage = "https://swapi.dev/api/starships/?page=" + currentPage;
+    getNextPageOfStarships(nextPage);
+})
+
+document.querySelector(".previous-button").addEventListener("click", () => {
+    currentPage -= 1;
+    let prevPage = "https://swapi.dev/api/starships/?page=" + currentPage;
+    getNextPageOfStarships(prevPage);
+})
