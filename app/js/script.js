6dda3aeb276d2ef80ef43d47e7ebc2ab88bf923e
Ajmal <7179908+ajmalesa@users.noreply.github.com>
2020-12-23 18:29:40 -0700

    Rewrote JavaScript to load correct pilots
    - Pilots loaded for starships were not matching after checking the API
    - Also the starships were not loading in the same order
    - Now using a recursive call from the first page and will keep checking
      as long as there are more pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,30 +1,46 @@
-for (let j = 1; j <= 3; j++) {
-    axios.get('https://swapi.dev/api/starships/?page=' + j)
-        .then((response) => {
-            response.data.results.forEach((starship, i) => {
-                document.querySelector("#list-of-starships").innerHTML += `
+// Initial page to look for starships
+let initialPage = "https://swapi.dev/api/starships/?page=1"
+
+// Function we will call with url of the next page of starships
+function getNextPageOfStarships(url) {
+    axios.get(url)
+
+    // If we get a response, append to page
+    .then((response) => {
+        for (let i = 0; i < response.data.results.length; i++) {
+            document.querySelector("#list-of-starships").innerHTML += `
                     <li class="starship">
-                        ${starship.name}
+                        ${response.data.results[i].name}
                     </li>
-                    <ol id="starships-for-${i * j}"></ol>
+                    <ul class="pilots-for-starships" id="pilots-for-${i + url[url.length-1]}"></ul>
                 `;
 
-                starship.pilots.forEach((pilot) => {
-                    axios.get(pilot).then((response) => {
-                        document.querySelector("#starships-for-" + (i * j)).innerHTML += `
-                            <ol>
-                                ${response.data.name}
-                            </ol>
-                        `;
-                    }).catch((err) => {
-                        document.querySelector("#starships-for-" + (i * j)).innnerHTML = "Could not get pilot. Contact administrator.";
-                        console.log(err);
-                    })
-                });
-            });
+            for (let j = 0; j < response.data.results[i].pilots.length; j++) {
+                axios.get(response.data.results[i].pilots[j])
+                    .then((pilot) => {
 
-        }).catch((err) => {
-            document.querySelector("#list-of-starships").innerHTML = "Could not get starship. Contact administrator.";
-            console.error(err)
-        });
+                        document.querySelector("#pilots-for-" + i + url[url.length - 1]).innerHTML += `
+                                <li>
+                                    ${pilot.data.name}
+                                </li>
+                            `;
+                    })
+            }
+        }
+
+        // As long as there is a next page of starships, recursively call function to get next page
+        if (response.data.next !== null) {
+            getNextPageOfStarships(response.data.next);
+        }
+
+    })
+
+    // If we are not able to get a response, let the user know and log the error to make it easy to find in devtools
+    .catch((err) => {
+        document.querySelector("#list-of-starships").innerHTML = "Could not get starship. Contact administrator.";
+        console.error(err);
+    });
 }
+
+// Call function defined above, which will keep being called as long as there are more pages of starships
+getNextPageOfStarships(initialPage);
